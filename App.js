72f55df17c79688cdcafef249106f23e8e3cabd6
ENZO MARTINS TYKALOWITZ <enzo_tykalowitz@estudante.sc.senai.br>
2023-04-13 21:17:57 -0300

    done

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import Formulario from './src/components/Formulario'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default (props) => (
+    <SafeAreaView>
+        <Formulario />
+    </SafeAreaView>
+)
